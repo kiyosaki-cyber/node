139ff4755b604ffa99d5612d33adcf7bf952eacb
kiyosaki-cyber <134641062+kiyosaki-cyber@users.noreply.github.com>
2023-07-04 23:52:40 +0300

    node-api

---
+++ node-api/app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const expressValidator = require("express-validator")
+const postRoutes = require("./routes/post")
+const dotenv =require("dotenv")
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI,
+    {useNewUrlParser:true} )
+.then(()=> console.log("DB CONNECTED"))
+mongoose.connection.on("error", err =>{
+    console.log(`DB connection error:${err.message}`)
+})
+app.use(morgan("dev"))
+app.use(bodyParser.json())
+app.use(expressValidator())
+app.use("/", postRoutes)
+const port = process.env.PORT || 8080
+app.listen(port, ()=>{
+    console.log(`a node.js is listening in port:${port}`)
+})
---
+++ node-api/routes/post.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const postController = require("../controllers/post")
+const validator = require("../helpers")
+const router = express.Router()
+router.get("/", postController.getPosts)
+router.post("/post", validator.createPostValidator, postController.createPost)
+module.exports = router
